bb704620900ca33935a75b869d50d8bc0be7758e
Mikolaj Radkowski <yknomeh@protonmail.com>
2018-11-27 00:37:28 +0100

    small fix

--- utils/utils.go
+++ utils/utils.go
@@ -16,7 +16,7 @@ func Colorize(text string, color string) string {
 		fmt.Println("\nFix that!")
 		return fmt.Sprintf("\x1b[38;2;%d;%d;%dm%s\x1b[0m", 255, 255, 255, text)
 	}
-	return fmt.Sprintf(`\x1b[38;2;%d;%d;%dm%s\x1b[0m`, int(c.R * 255), int(c.G * 255), int(c.B * 255), text)
+	return fmt.Sprintf("\x1b[38;2;%d;%d;%dm%s\x1b[0m", int(c.R * 255), int(c.G * 255), int(c.B * 255), text)
 }
 
 func GetWorkingDirectory(homeParse bool) (string, []string) {
